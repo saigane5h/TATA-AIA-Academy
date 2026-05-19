f86aa4f4bb8bc88ef044223dd48c61cbc036586b
saigane5h <motamarrisaiganesh@gmail.com>
2026-05-19 18:44:03 +0530

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'img.youtube.com' },
-      { protocol: 'https', hostname: 'i.ytimg.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
   },
